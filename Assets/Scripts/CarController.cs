eb0c5ade983d02cb647a0d03e8929fabaff9e395
yoshimoridao <yoshimoridao@gmail.com>
2020-10-22 14:06:17 +0700

    * modified control

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,8 +5,16 @@ using System;
 
 public class CarController : MonoBehaviour
 {
+    public static CarController Instance;
     public static Action<bool> actOnControlWheel;
+    public static Action<bool> actOnControlGas;
     public static Action actOnReleaseWheel;
+    public static Action actOnReleaseGas;
+
+    private static float LERP_STRAIGHT_WHEEL = 0.1f;
+    private static float LERP_TURN_WHEEL = 10.0f;
+    private static float MVM_SPEED = 1.0f;
+    private static float LERP_STOP = 0.5f;
 
     [SerializeField]
     Transform wheelFrontRight;
@@ -15,44 +23,84 @@ public class CarController : MonoBehaviour
     [SerializeField]
     public int maxRotationY;
 
-    private float wheelHoldTime = 0.0f;
     private bool isTurningLeft = false;
     private bool isTurningRight = false;
+    private float xVel = 0.0f;
+
+    private bool isPressGas = false;
+    private bool isBrake = false;
+    private float zVel = 0.0f;
 
     private void Awake()
     {
+        Instance = this;
+
         actOnControlWheel += OnControlWheel;
         actOnReleaseWheel += OnReleaseWheel;
+
+        actOnControlGas += OnControlGas;
+        actOnReleaseGas += OnReleaseGas;
     }
 
     void Start()
     {
-        
     }
 
     void Update()
     {
+        // control x velocity
         if (isTurningRight || isTurningLeft)
         {
-            wheelHoldTime += (isTurningRight ? 1 : -1) * Time.deltaTime;
-            wheelHoldTime = Mathf.Clamp(wheelHoldTime, -1.0f, 1.0f);
+            xVel = Mathf.Clamp(xVel + ((isTurningRight ? 1 : -1) * Time.deltaTime * LERP_TURN_WHEEL), -1.0f, 1.0f);
         }
-        else if (wheelHoldTime != 0)
+        else if (xVel != 0)
         {
-            wheelHoldTime = Mathf.Lerp(wheelHoldTime, 0.0f, 0.1f);
+            xVel = Mathf.Lerp(xVel, 0.0f, LERP_STRAIGHT_WHEEL);
         }
 
-        float rotateY = maxRotationY * wheelHoldTime;
+        // rotate wheels
+        float rotateY = maxRotationY * xVel;
         var angle = wheelFrontRight.transform.eulerAngles;
         angle.y = rotateY;
         wheelFrontRight.transform.eulerAngles = angle;
         wheelFrontLeft.transform.eulerAngles = angle;
+
+        // control z velocity
+        if (isBrake || isPressGas)
+        {
+            zVel = Mathf.Clamp(zVel + ((isPressGas ? 1 : -1) * Time.deltaTime * LERP_TURN_WHEEL), -1.0f, 1.0f);
+        }
+        else if (zVel != 0)
+        {
+            zVel = Mathf.Lerp(zVel, 0.0f, LERP_STOP);
+        }
+
+        // update position
+        if (zVel != 0)
+        {
+            var pos = transform.position;
+            pos.x += xVel * 0.01f;
+            pos.z += zVel * 0.01f;
+            transform.position = pos;
+        }
     }
 
     private void OnDestroy()
     {
         actOnControlWheel -= OnControlWheel;
         actOnReleaseWheel -= OnReleaseWheel;
+
+        actOnControlGas -= OnControlGas;
+        actOnReleaseGas -= OnReleaseGas;
+    }
+
+    public bool IsControlGas()
+    {
+        return isPressGas || isBrake;
+    }
+    public bool IsControlWheel()
+    {
+        return isTurningLeft || isTurningRight;
     }
 
     private void OnControlWheel(bool isOnTurnRight)
@@ -60,7 +108,6 @@ public class CarController : MonoBehaviour
         isTurningRight = isOnTurnRight;
         isTurningLeft = !isOnTurnRight;
     }
-
     private void OnReleaseWheel()
     {
         if (isTurningLeft)
@@ -68,5 +115,19 @@ public class CarController : MonoBehaviour
 
         if (isTurningRight)
             isTurningRight = false;
+    }
+
+    private void OnControlGas(bool isPressGas)
+    {
+        this.isPressGas = isPressGas;
+        this.isBrake = !isPressGas;
+    }
+    private void OnReleaseGas()
+    {
+        if (isPressGas)
+            isPressGas = false;
+
+        if (isBrake)
+            isBrake = false;
     }
 }
---
+++ Assets/Scripts/InputMgr.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMgr : MonoBehaviour
+{
+    private CarController carController;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (carController == null)
+            carController = CarController.Instance;
+
+        // control wheel
+        if (Input.GetKey(KeyCode.A))
+        {
+            CarController.actOnControlWheel(false);
+            Debug.Log("press A");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            CarController.actOnControlWheel(true);
+        }
+        else if (carController && carController.IsControlWheel())
+        {
+            CarController.actOnReleaseWheel();
+        }
+
+        // control gas
+        if (Input.GetKey(KeyCode.W))
+        {
+            CarController.actOnControlGas(true);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            CarController.actOnControlGas(false);
+        }
+        else if (carController && carController.IsControlGas())
+        {
+            CarController.actOnReleaseGas();
+        }
+    }
+}
